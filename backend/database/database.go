823446459a3d8b761fb12c19d52a4fe274cada4c
u210645 <florian.thievent@sbb.ch>
2023-12-06 17:21:27 +0100

    added gorm migration check

--- backend/database/database.go
+++ backend/database/database.go
@@ -9,12 +9,17 @@ import (
 	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
 	"go.uber.org/zap"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"spoutmc/backend/docker"
 	"spoutmc/backend/log"
 	"spoutmc/backend/models"
 	"spoutmc/backend/utils"
+	"spoutmc/backend/watchdog"
+	"time"
 )
 
 // Example docker command
@@ -27,12 +32,64 @@ var databaseContainerName = "spoutdatabase"
 var databaseContainerImage = "mysql"
 var logger = log.New()
 
+// Todo remove this one, only to check
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
 func Start() {
 	docker.PullImage(databaseContainerImage)
+
+	containerId := ""
+
 	if checkHasDatabaseContainer() {
-		restartDatabaseContainer()
+		containerId = restartDatabaseContainer()
 	} else {
-		createDatabaseContainer()
+		containerId = createDatabaseContainer()
+	}
+
+	watchdog.AddToWatchdog(containerId)
+
+	go migrateDatabase(containerId)
+
+}
+
+func migrateDatabase(containerId string) {
+	for {
+		// Get container information
+		containerInfo, err := cli.ContainerInspect(ctx, containerId)
+		if err != nil {
+			logger.Error("", zap.Error(err))
+		}
+
+		// Check if the container is in the "Running" state
+		logger.Info(fmt.Sprintf("Database Status %s", containerInfo.State.Status))
+
+		if containerInfo.State.Status == "running" {
+			time.Sleep(5 * time.Second)        // Todo need here a
+			pw, err := getDbPasswordIfExists() // Todo extend this wit configurtion properties or ENV Var
+			if err != nil {
+				logger.Error("Cannot get DatabasePassword from File")
+			}
+
+			dsn := fmt.Sprintf("spoutdbuser:%s@tcp(127.0.0.1:3306)/spout?charset=utf8mb4&parseTime=True&loc=Local", pw)
+			db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+			if err != nil {
+				logger.Error("", zap.Error(err))
+			}
+			// Migrate the schema
+			err = db.AutoMigrate(&Product{})
+			if err != nil {
+				logger.Error("", zap.Error(err))
+			}
+			logger.Info("Applied migrations to Database")
+			break
+		}
+
+		// Sleep for a short duration before checking again
+		time.Sleep(1 * time.Second)
 	}
 }
 
@@ -51,7 +108,7 @@ func checkHasDatabaseContainer() bool {
 	return false
 }
 
-func restartDatabaseContainer() {
+func restartDatabaseContainer() string {
 	databaseContainer := docker.GetContainer(databaseContainerName)
 
 	if databaseContainer.State == "exited" {
@@ -65,12 +122,14 @@ func restartDatabaseContainer() {
 			logger.Error(err.Error())
 		}
 	}
+
+	return databaseContainer.ID
 }
 
-func writeDBPassword(password string) {
+func writeDBPassword(password string) string {
 	wd, err := os.Getwd()
 	if err != nil {
-		return
+		return ""
 	}
 
 	path := filepath.Join(wd, ".msql_password")
@@ -90,12 +149,40 @@ func writeDBPassword(password string) {
 		logger.Error("", zap.Error(err))
 	}
 	logger.Info(fmt.Sprintf("Find your DB Password in %s. Do not forget to delete the file from the filesystem", path))
+	return path
 }
 
-func createDatabaseContainer() {
+func getDbPasswordIfExists() (string, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	path := filepath.Join(wd, ".msql_password")
+
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+
+	defer file.Close()
+
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		return "", err
+	}
+
+	return string(content), nil
+
+}
+
+func createDatabaseContainer() string {
 	logger.Info(fmt.Sprintf("Creating database container %s", databaseContainerName))
-	rootPassword := utils.RandomString(25)
-	writeDBPassword(rootPassword)
+	rootPassword, err := getDbPasswordIfExists()
+	if err != nil {
+		rootPassword = utils.RandomString(25)
+		writeDBPassword(rootPassword)
+	}
 
 	exposedPorts, containerPortBinding := docker.MapExposedPorts(models.SpoutServerPorts{
 		HostPort:      "3306",
@@ -107,10 +194,11 @@ func createDatabaseContainer() {
 		"io.spout.network":    "true",
 	}
 
+	// Todo change password to MYSQL_PASSWORD_FILE and mount that file
 	databaseContainer, err := cli.ContainerCreate(ctx, &container.Config{
 		Image:        databaseContainerImage,
 		Hostname:     databaseContainerName,
-		Env:          []string{fmt.Sprintf("MYSQL_ROOT_PASSWORD=%s", rootPassword)},
+		Env:          []string{fmt.Sprintf("MYSQL_ROOT_PASSWORD=%s", rootPassword), fmt.Sprintf("MYSQL_PASSWORD=%s", rootPassword), "MYSQL_USER=spoutdbuser", "MYSQL_DATABASE=spout"},
 		ExposedPorts: exposedPorts,
 		Labels:       containerLabels,
 	}, &container.HostConfig{
@@ -125,6 +213,8 @@ func createDatabaseContainer() {
 	if err := cli.ContainerStart(ctx, databaseContainer.ID, types.ContainerStartOptions{}); err != nil {
 		logger.Error("Cannot start database container", zap.Error(err))
 	}
+
+	return databaseContainer.ID
 
 }
 
